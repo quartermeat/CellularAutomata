aeff53795327952ef68111655de3e3417d4676ff
quartermeat <jeremy.williamson83@gmail.com>
2016-10-01 11:11:21 -0400

    Added some members and a constructor to the Map class.

--- CellularAutomata/Model/Map.cs
+++ CellularAutomata/Model/Map.cs
@@ -3,5 +3,18 @@
     //map should keep track of the state of the spaces on the gameboard as well as be used to track cells
     class Map
     {
+        //how many squares horizontally
+        public int Width;
+        //how many squares vertically
+        public int Height;
+        //how big are the squares
+        public int Resolution;
+
+        public Map()
+        {
+            Width = 100;
+            Height = 100;
+            Resolution = 10;
+        }
     }
 }
